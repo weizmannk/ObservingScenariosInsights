f5921cc3ad9777440d4df1906dbf3fb646664e1c
Weizmann <kiend.weizman7@gmail.com>
2023-12-18 19:13:45 +0100

    Add plot function

--- src/BNS_Inspiral_Range.py
+++ src/BNS_Inspiral_Range.py
@@ -18,6 +18,40 @@ from gwpy.astro import inspiral_range
 import numpy as np
 from pathlib import Path
 
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+# Set style
+sns.set_style("whitegrid")
+
+# Set font and text properties
+plt.rcParams.update(
+    {
+        "font.family": "serif",
+        "font.serif": ["Times New Roman"],
+        "text.usetex": True,
+        "font.size": 12,
+        "legend.fontsize": 12,
+        "axes.labelsize": 12,
+        "axes.titlesize": 14,
+        "xtick.labelsize": 10,
+        "ytick.labelsize": 10,
+        "text.latex.preamble": r"\usepackage{amsmath}",
+        "figure.figsize": (6, 4),  # Set your desired figure size
+        "savefig.dpi": 300,
+    }
+)
+
+# Ensure grid is behind other plot elements
+plt.rc("axes", axisbelow=True)
+
+
+colors = {"L1": "#4ba6ff", "H1": "#9b59b6", "V1": "#ee0000", "K1": "#00FF00"}
+
+
+print(colors)  # Display the updated dictionary
+
 
 class BNSInspiralRangeCalculator:
     def __init__(self, run_names, ifos, path, data_type="ASD"):
@@ -113,6 +147,83 @@ class BNSInspiralRangeCalculator:
                     continue
         print("\n======================================\n")
 
+    def plot_sensitivity_curves(self, run_names, outdir):
+        fig = plt.figure(figsize=(4.5, 3))
+
+        dashed_handles = []  # Handles for dashed lines
+        dashed_labels = []  # Labels for dashed lines
+        solid_handles = []  # Handles for solid lines
+        solid_labels = []  # Labels for solid lines
+
+        # Initialize labels for detectors
+        labels = {ifo: True for ifo in self.ifos}
+
+        for run_name in run_names:
+            print("\n======================================\n")
+            print(f"The BNS range in Run {run_name},")
+            if run_name == "O4a":
+                print("with the Measured PSD\n")
+                linestyle = "--"  # Use dashed lines for O4a detectors
+            else:
+                print("with the Ideal PSD\n")
+                linestyle = "-"  # Use solid lines for other runs
+
+            for ifo in self.ifos:
+                if ifo in self.sensitivity_files.get(run_name):
+                    freq, psd = self.load_data(run_name, ifo)
+                    asd = np.sqrt(psd)
+
+                    label = f"{ifo} {'O4a' if run_name == 'O4a' else 'O4b'}"
+
+                    plt.loglog(
+                        freq,
+                        asd,
+                        label=label,
+                        color=colors[ifo],
+                        linewidth=1,
+                        alpha=0.7,
+                        linestyle=linestyle,
+                    )
+
+                    if linestyle == "--":
+                        dashed_handles.append(
+                            plt.Line2D(
+                                [0, 1], [0, 1], color=colors[ifo], linestyle=linestyle
+                            )
+                        )
+                        dashed_labels.append(label)
+                    else:
+                        solid_handles.append(
+                            plt.Line2D(
+                                [0, 1], [0, 1], color=colors[ifo], linestyle=linestyle
+                            )
+                        )
+                        solid_labels.append(label)
+
+        legend1 = plt.legend(
+            dashed_handles, dashed_labels, loc="upper left", title="O4a"
+        )
+        legend2 = plt.legend(
+            solid_handles, solid_labels, loc="lower right", title="O4b"
+        )
+        plt.gca().add_artist(legend1)  # Ensure both legends are displayed
+
+        plt.xlabel(r"$\mathrm{Frequency}\,\mathrm{[Hz]}$")
+        plt.ylabel(r"$\mathrm{ASD}\,[1/\sqrt{\mathrm{Hz}}]$")
+        plt.xlim([10, 4000])
+        plt.ylim(1e-24, 1e-20)
+
+        plt.grid()
+        fig.tight_layout()
+        plt.savefig(f"{outdir}/Strain_HLV.png", dpi=300)
+        plt.close()
+
+
+# Outdir
+
+outdir = "../Plots"
+if not os.path.isdir(outdir):
+    os.makedirs(outdir)
 
 # the runs data direction
 path = Path("../data/ASD")
@@ -125,3 +236,5 @@ calculator = BNSInspiralRangeCalculator(
     run_names=run_names, ifos=IFOs, path=path, data_type=data_type
 )
 calculator.calculate_bns_range()
+
+calculator.plot_sensitivity_curves(run_names=["O4a", "O4b"], outdir=outdir)
